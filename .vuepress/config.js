7e01f2125cbd571cb66ef52efc5c9d998b9fd1c5
xielongmei <15577506835@163.com>
2023-08-01 10:19:16 +0800

    feat: 测试action

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,96 +1,96 @@
 module.exports = {
-  base: "/blog-test/",
+  base: '/blog/',
   // base: "/",
-  "title": "yisumms",
-  "description": "我的个人博客",
-  "dest": "public",
-  "head": [
-    [
-      "link",
-      {
-        "rel": "icon",
-        "href": "/favicon.ico"
-      }
-    ],
-    [
-      "meta",
-      {
-        "name": "viewport",
-        "content": "width=device-width,initial-scale=1,user-scalable=no"
-      }
-    ]
-  ],
-  "theme": "reco",
-  "themeConfig": {
-    "nav": [
-      {
-        "text": "Home",
-        "link": "/",
-        "icon": "reco-home"
-      },
-      {
-        "text": "TimeLine",
-        "link": "/timeline/",
-        "icon": "reco-date"
-      },
-      {
-        "text": "Docs",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "vuepress-reco",
-            "link": "/docs/theme-reco/"
-          }
-        ]
-      },
-      {
-        "text": "Contact",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "GitHub",
-            "link": "https://github.com/xielongmei",
-            "icon": "reco-github"
-          }
-        ]
-      }
-    ],
-    "sidebar": {
-      "/docs/theme-reco/": [
-        "",
-        "theme",
-        "plugin",
-        "api"
-      ]
-    },
-    "type": "blog",
-    "blogConfig": {
-      "category": {
-        "location": 2,
-        "text": "Category"
-      },
-      "tag": {
-        "location": 3,
-        "text": "Tag"
-      }
-    },
-    "friendLink": [
-      {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
-      }
-    ],
-    "logo": "/logo.png",
-    "search": true,
-    "searchMaxSuggestions": 10,
-    "lastUpdated": "Last Updated",
-    "author": "yisumms",
-    "authorAvatar": "/avatar.jpg",
-    "startYear": "2022"
+  title: 'yisumms',
+  description: '悟已往之不谏，知来者之可追',
+  descriptionStyle: {
+    color: '#fff',
   },
-  "markdown": {
-    "lineNumbers": true
-  }
-}
+  dest: 'public',
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/favicon.ico',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'viewport',
+        content: 'width=device-width,initial-scale=1,user-scalable=no',
+      },
+    ],
+  ],
+  theme: 'reco',
+  themeConfig: {
+    nav: [
+      {
+        text: 'Home',
+        link: '/',
+        icon: 'reco-home',
+      },
+      {
+        text: 'TimeLine',
+        link: '/timeline/',
+        icon: 'reco-date',
+      },
+      {
+        text: 'Docs',
+        icon: 'reco-message',
+        items: [
+          {
+            text: 'vuepress-reco',
+            link: '/docs/theme-reco/',
+          },
+        ],
+      },
+      {
+        text: 'Contact',
+        icon: 'reco-message',
+        items: [
+          {
+            text: 'GitHub',
+            link: 'https://github.com/xielongmei',
+            icon: 'reco-github',
+          },
+        ],
+      },
+    ],
+    sidebar: {
+      '/docs/theme-reco/': ['', 'theme', 'plugin', 'api'],
+    },
+    type: 'blog',
+    blogConfig: {
+      category: {
+        location: 2,
+        text: 'Category',
+      },
+      tag: {
+        location: 3,
+        text: 'Tag',
+      },
+    },
+    friendLink: [
+      {
+        title: 'vuepress-theme-reco',
+        desc: 'A simple and beautiful vuepress Blog & Doc theme.',
+        avatar:
+          'https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png',
+        link: 'https://vuepress-theme-reco.recoluan.com',
+      },
+    ],
+    logo: '/logo.png',
+    search: true,
+    searchMaxSuggestions: 10,
+    lastUpdated: 'Last Updated',
+    author: 'yisumms',
+    authorAvatar: '/avatar.jpg',
+    startYear: '2022',
+  },
+  markdown: {
+    lineNumbers: true,
+    highlightLines: true,
+  },
+};
